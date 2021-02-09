ba159530a724dcddbd0b0c6b49dd482e181565d0
hegde <anupama.hegde@carissma.eu>
2021-02-09 09:00:38 +0100

    MovingNodeController

---
+++ src/artery/traci/MovingNodeController.cc
@@ -0,0 +1,16 @@
+/*
+ * NodeController.cc
+ *
+ *  Created on: Aug 5, 2020
+ *      Author: vm-sts
+ */
+
+#include <artery/traci/MovingNodeController.h>
+
+namespace traci {
+
+MovingNodeController::MovingNodeController() { }
+
+MovingNodeController::~MovingNodeController() { }
+
+} /* namespace traci */
